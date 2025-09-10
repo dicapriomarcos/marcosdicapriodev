5fb48076b8d828099efe83644bdf3563e49297f0
Marcos Di Caprio <marcos@trestristestigres.com>
2025-09-11 01:42:52 +0200

    header styles

--- header.php
+++ header.php
@@ -7,27 +7,22 @@
 </head>
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
-    <header id="site-header" role="banner">
-        <div class="container">
-            <div class="site-branding">
-                <?php
-                if ( has_custom_logo() ) {
-                    the_custom_logo();
-                } else {
-                    echo '<h1 class="site-title"><a href="' . esc_url( home_url( '/' ) ) . '">' . get_bloginfo( 'name' ) . '</a></h1>';
-                    echo '<p class="site-description">' . get_bloginfo( 'description' ) . '</p>';
-                }
-                ?>
-            </div>
-            <nav id="site-navigation" class="main-navigation" role="navigation">
-                <?php
+    <header class="md__header" role="banner">
+        <div class="md__container">
+            <div class="header-wrapper">
+                <div class="md__site-logo">
+                    <h1> <a href="<?php echo esc_url(home_url('/')) ?>">marcosdicaprio.dev</a></h1>
+                </div>
+                <nav id="site-navigation" class="md__main-navigation" role="navigation">
+                    <?php
                 wp_nav_menu( array(
                     'theme_location' => 'primary',
                     'menu_id'        => 'primary-menu',
                     'container'      => false,
                 ) );
                 ?>
-            </nav>
+                </nav>
+            </div>
         </div>
     </header>
     </body>
